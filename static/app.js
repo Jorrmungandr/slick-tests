3ed07e13a39d27902db52775ce15413bf30a31fc
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-10 22:02:34 -0300

    Added Second Slider

--- static/app.js
+++ static/app.js
@@ -1,8 +1,4 @@
 $(document).ready(() => {
-  // document.querySelector('.carousel').innerHTML += `
-  //   <div class="prev arrow"></div>
-  //   <div class="next arrow"></div>
-  // `;
   $('.carousel').slick({
     slidesToShow: 1,
     dots: true,
@@ -12,6 +8,14 @@ $(document).ready(() => {
     prevArrow: '<a class="prev arrow fa fa-angle-left"></a>',
     nextArrow: '<a class="next arrow fa fa-angle-right"></a>',
   });
+  $('.carousel1').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    customPaging: function (slider, i) {
+      return '<div class="dot" id=' + i + "></div>";
+    },
+    prevArrow: '<a class="prev arrow fa fa-angle-left"></a>',
+    nextArrow: '<a class="next arrow fa fa-angle-right"></a>',
+  });
 });
-document.addEventListener('click', () => {
-})
